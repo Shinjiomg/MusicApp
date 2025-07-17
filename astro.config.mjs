1e46c66a20446ca049eab45172ac5a4b7b75d6d9
Shinjiomg <48720513+Shinjiomg@users.noreply.github.com>
2025-07-17 12:34:07 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
   integrations: [solidJs()],
   vite: {
     plugins: [tailwindcss()]
