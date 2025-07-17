6b6a7f3cff257330641d83586070711de2655c2b
Shinjiomg <48720513+Shinjiomg@users.noreply.github.com>
2025-07-17 12:21:04 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
-import dotenv from 'dotenv';
-
-// Cargar variables de entorno
-dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
